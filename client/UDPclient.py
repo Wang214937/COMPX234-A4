f9fbddb0ccb132ffaafce7b1f5281d6fbdca1dec
Wang Jiarui <2766259133@qq.com>
2025-05-29 22:36:36 +0800

    import socket then create class UDPClient and the init method

---
+++ client/UDPclient.py
@@ -0,0 +1,8 @@
+import socket
+
+class UDPclient:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
