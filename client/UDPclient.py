6abb663d569cfa2d1bba4556c790884a1e164a28
Wang Jiarui <2766259133@qq.com>
2025-05-29 23:07:14 +0800

    add file list in the init

--- client/UDPclient.py
+++ client/UDPclient.py
@@ -1,10 +1,11 @@
 import socket
 
 class UDPclient:
-    def __init__(self, ip, port):
+    def __init__(self, ip, port,file_list):
         self.ip = ip
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.file_list = self._load_file_list(file_list)
 
     def _load_file_list(self, file_list):
         with open(file_list, 'r') as f:
