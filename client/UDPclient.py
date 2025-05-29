520856da2d408e1d13f8ce15b14dec38aa7c530a
Wang Jiarui <2766259133@qq.com>
2025-05-29 23:12:47 +0800

    create a new method send_files

--- client/UDPclient.py
+++ client/UDPclient.py
@@ -10,3 +10,11 @@ class UDPclient:
     def _load_file_list(self, file_list):
         with open(file_list, 'r') as f:
             return [line.strip() for line in f if line.strip()]
+
+    def send_files(self): 
+        for file_name in self.file_list:
+            with open(file_name, 'rb') as f:
+                data = f.read()
+                self.sock.sendto(data, (self.ip, self.port))
+                print(f"Sent {file_name} to {self.ip}:{self.port}")
+        self.sock.close()
