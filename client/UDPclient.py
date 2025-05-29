800511210dd08daeddbee9596963a7c6c3216d66
Wang Jiarui <2766259133@qq.com>
2025-05-29 23:04:52 +0800

    create a new method _load_file_list ,used to read file

--- client/UDPclient.py
+++ client/UDPclient.py
@@ -6,3 +6,6 @@ class UDPclient:
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    def _load_file_list(self, file_list):
+        with open(file_list, 'r') as f:
+            return [line.strip() for line in f if line.strip()]
